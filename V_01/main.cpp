040c11c3acb9af05d6f220e62cec566b02e70a4c
root <root@localhost.localdomain>
2019-03-05 17:51:01 +0000

    nfillim i bahet qmake datei me endung .pro manejna i ban make asaj .cpp

--- V_01/main.cpp
+++ V_01/main.cpp
@@ -4,6 +4,7 @@
 int main(int argc,char **argv){
     QApplication app(argc,argv);
     QPushButton button ("Hello");
+    QPushButton button2 ("Aron",&button);
     button.show();
     std::cout<< "He ma njish";
     return app.exec();
